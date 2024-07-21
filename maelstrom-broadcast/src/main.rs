bdc8d664b13103d5d243f4abfa51e024280d7411
soltybeans <soltybeans@gmail.com>
2024-07-21 15:37:58 +0200

    Working single node broadcast

--- maelstrom-broadcast/src/main.rs
+++ maelstrom-broadcast/src/main.rs
@@ -1,11 +1,9 @@
 // cargo build && ~/maelstrom.tar/maelstrom/maelstrom test -w broadcast --bin ./target/debug/maelstrom-broadcast --node-count 1 --time-limit 20 --rate 10
-use std::collections::{BTreeSet, HashSet};
 use async_trait::async_trait;
-use maelstrom::protocol::{Message};
+use maelstrom::protocol::{Message, MessageBody};
 use maelstrom::{done, Node, Result, Runtime};
 use std::sync::Arc;
-use serde_json::{json};
-use tokio::sync::mpsc::{channel, Sender};
+use serde_json::{Value};
 use tokio::sync::Mutex;
 
 pub(crate) fn main() -> Result<()> {
@@ -13,27 +11,16 @@ pub(crate) fn main() -> Result<()> {
 }
 
 async fn try_main() -> Result<()> {
-    let (tx, mut receiver) = channel(1);
-    let handler = Arc::new(Handler { sender: tx, seen: Arc::new(Mutex::new(Vec::new())) });
+    let handler = Arc::new(Handler { seen: Arc::new(Mutex::new(Vec::new())) });
     //let mut seen = HashSet::new();
     Runtime::new().with_handler(handler).run().await.unwrap();
-
-    // let mut msg: u64 = 0;
-    // thread::scope(|s| {
-    //     s.spawn(|| {
-    //         // This probably shouldn't be blocking?!
-    //         msg = receiver.blocking_recv().unwrap();
-    //         seen.insert(msg);
-    //     });
-    // });
 
     Ok(())
 }
 
 #[derive(Clone)]
 struct Handler {
-    sender: Sender<u64>,
-    seen: Arc<Mutex<Vec<u64>>>,
+    seen: Arc<Mutex<Vec<Value>>>,
 }
 
 #[async_trait]
@@ -50,21 +37,26 @@ impl Node for Handler {
 
 async fn process_broadcast_message(handler: &Handler, req: Message, runtime: Runtime) -> Result<()> {
     let msg = req.body.extra.get("message").unwrap().as_number().unwrap().to_owned();
-    let msg = msg.as_u64().unwrap();
-    handler.seen.lock().await.push(msg);
+    //let msg = msg.as_u64().unwrap();
+    handler.seen.lock().await.push(Value::from(msg));
     //handler.sender.send(msg).await.expect("Expected to store msg");
-    let message_body = req.body.clone().with_type("broadcast_ok");
+    let message_body = MessageBody::new()
+        .and_msg_id(req.body.msg_id)
+        .with_type("broadcast_ok");
     runtime.reply(req, message_body).await
 }
 
 async fn process_read_message(handler: &Handler, req: Message, runtime: Runtime) -> Result<()> {
-    let message_body = req.body.clone()
-        .with_type("read_ok")
-        .extra.insert(String::from("messages"), json!(handler.seen.lock().await.to_vec()));
+    let mut message_body = MessageBody::new()
+        .and_msg_id(req.body.msg_id)
+        .with_type("read_ok");
+    message_body.extra.insert(String::from("messages"), serde_json::value::Value::Array(handler.seen.lock().await.clone()));
     runtime.reply(req, message_body).await
 }
 
 async fn process_topology_message(req: Message, runtime: Runtime) -> Result<()> {
-    let message_body = req.body.clone().with_type("topology_ok");
-    runtime.reply(req, message_body).await
+    let msg = MessageBody::new()
+        .and_msg_id(req.body.msg_id)
+        .with_type("topology_ok");
+    runtime.reply(req, msg).await
 }
